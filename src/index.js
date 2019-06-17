6f7ef1fd07063554182b5483683d8cf130a879fc
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-06-17 19:59:38 +0300

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import rootReducer from './reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import game from './shared/reducers';
+import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer)
+let store = createStore(game)
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+registerServiceWorker();
 
