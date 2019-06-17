4ed6f308aeed07e081304a5952c004513213bc6b
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-06-17 20:06:42 +0300

    action update

---
+++ src/component/GameConfigurations.jsx
@@ -0,0 +1,17 @@
+export const conf1 = {
+    rock: { beats: ["scissors"] },
+    paper: { beats: ["rock"] },
+    scissors: { beats: ["paper"] }
+}
+
+export const conf2 = {
+    "rock": {
+        "beats": ["scissors"]
+    },
+    "paper": {
+        "beats": ["rock"]
+    },
+    "scissors": {
+        "beats": ["paper"]
+    }
+}
