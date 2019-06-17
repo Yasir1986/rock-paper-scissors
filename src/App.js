05b5f179bd6bd1df867cfd9735aee204a44f09bc
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-06-17 20:04:17 +0300

    score added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Home from './component/Home'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import HumanVSCompContainer from './humanVsComp/HumanVSCompContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Rock Paper Sissors</h1>
-      <Home />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>  
+          <main>
+            <h1>Rock Paper Scissors</h1>
+            <Route exact path="/" component={HumanVSCompContainer}/>
+          </main>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
